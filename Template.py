395f95e074546455e59b5b238059541ca46c629d
Bill Baker <bill@baker88.com>
2021-08-24 13:22:32 -0700

    version 1 8/24/2021

---
+++ Template.py
@@ -0,0 +1,20 @@
+"""
+NTEC 361
+date: <ex: mm/dd/yyyy>
+<your name>
+Name: <assignment/lab name>
+Description: <assignment/lab description>
+"""
+
+""" IPO template - use to document functions/methods
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+
+def main():
+    # code here
+
+
+main()
